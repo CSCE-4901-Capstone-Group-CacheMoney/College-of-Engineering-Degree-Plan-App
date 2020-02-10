455e183c10974424708604a3b9be74964b755dcb
tmosier155 <tatemosier@my.unt.edu>
2020-02-10 14:07:40 -0600

    added everythng back, including the envname folder where the code will execute

---
+++ src/envname/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Tate Mosier\Downloads\OptimizePrime-master\OptimizePrime-master\src\environmentName\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
