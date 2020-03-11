f70e8022238a9468f8d43c84455081ba3d0ad2a9
Stephen10023 <stephenhostar@sbcglobal.net>
2020-03-11 16:55:35 -0500

    deleted duplicate env that was pushed

--- environmentName/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\hp\desktop\unt\csce4901\college-of-engineering-degree-plan-app\environmentname\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
