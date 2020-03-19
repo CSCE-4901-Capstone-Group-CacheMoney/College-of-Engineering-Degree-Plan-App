575ff9777ade824c59a6a885745ac53533f38ad7
tmosier155 <tatemosier@my.unt.edu>
2020-03-18 20:03:14 -0500

    Completes front-end add degree with dynamic catagories and courses with autocomplete. All degree information is placed into a nested json and POSTed to the back-end.

--- src/static/js/administration/degree-plans.js
+++ src/static/js/administration/degree-plans.js
@@ -14,14 +14,67 @@ $(document).ready(function() {
 
 
 	/*-----------------------add degree js----------------------------------------- */
+	// auto complete for courses
+    $('#degree-catagories').on('keypress', '.add-course-input', function(e) {
+    	// force uppercase
+    	$(this).val(sanatize($(this).val().toUpperCase()));
+     	var searchText = sanatize($(this).val().trim());
+		// auto complete search for view course
+		$(this).autocomplete({
+		    lookup: function (query, done) {
+				var result;
+		        $.post("/administration/view-course/js/",
+			    {
+			      courseSearchText: sanatize(searchText)
+				},
+			    function(data,status) {
+					if(!$.isEmptyObject(data)) {
+						result = {
+			            	suggestions: [
+								{"value": data.CourseDept + " " +  data.CourseID + "-"+ data.CourseName}
+			            	]
+						};
+						done(result);
+					} else {
+						result = { suggestions: [] };
+						done(result);
+					}
+				});
+		    },
+		    onSelect: function (suggestion) {
+		        $(this).val(suggestion.value.split("-")[0].trim());
+		    }
+		});
+	});
+
 	$(document).on("click", "#add-degree-submit-btn", function(e) {
+
+		var jsonResponse = {};
+		jsonResponse["Catagories"] = {};
+
+		$('.add-catagory').each(function(index) {
+
+			var catagory = sanatize($(this).children("input").eq(0).val().trim());
+			var requireHours = parseInt(sanatize($(this).children("input").eq(1).val().trim()));
+
+			jsonResponse["Catagories"][catagory] = {};
+			jsonResponse["Catagories"][catagory]["Required Hours"] = requireHours;
+			jsonResponse["Catagories"][catagory]["Courses"] = [];
+
+			$(this).children(".catagory-courses").children("tr").each(function(index) {
+				var course = sanatize($(this).children().children().children("input").val().trim());
+				jsonResponse["Catagories"][catagory]["Courses"][index] = course;
+			});
+
+		});
+
     	// send request to the back-end...
 		$.post("/administration/add-degree/js/",
    		{
-   			// nDegreeCollegeName: $("#degreeaddinputGroupSelect-1").val().trim(),
+   			nDegreeCollegeName: $("#degreeaddinputGroupSelect-4").val().trim(),
    			nDegreeName: $("#degreeaddinputGroupSelect-2").val().trim(),
 			ncatalogYear: $("#degreeaddinputGroupSelect-3").val().trim(),
-			ndegreeInfo: $("#degreeaddinputGroupSelect-4").val().trim()
+			ndegreeInfo: JSON.stringify(jsonResponse)
    		},
    		function(data,status) {
    			if(data.success.toLowerCase().indexOf("true") != -1) {
@@ -46,6 +99,51 @@ $(document).ready(function() {
     // also capture enter key to trigger above function
     $("#degreeaddinputGroupSelect-4").keypress(function(e) {
         if(e.which == 13) { $("#add-degree-submit-btn").click(); }
+    });
+
+    // for adding degree catagories
+    $(document).on("click", "#add-degree-catagory-btn", function(e) {
+    	var html = '<div class="add-catagory input-group mb-3">' +
+    			   '<div class="input-group-prepend">' +
+    			   '<span class="input-group-text" style="padding-right: .5em;">' +
+    			   '<i class="fa fa-trash remove-catagory mr-1"></i>Catagory Name</span>' +
+    			   '</div>' +
+    			   '<input type="text" class="form-control">' +
+    			   '<div class="w-100">' +
+    			   '</div>' +
+    			   '<div class="input-group-prepend mt-2 mb-1 ml-3">' +
+    			   '<span class="input-group-text" style="padding-right: 1.0em;">' +
+    			   'Required Courses</span>' +
+    			   '</div>' +
+    			   '<input type="text" class="form-control mt-2">' +
+    			   '<table class="catagory-courses w-100">' +
+    			   '</table>' +
+    			   '<button type="button" class="add-catagory-course btn btn-outline-success mb-1 mt-1 ml-5">Add Course</button>' +
+    			   '</div>';
+    	$("#degree-catagories").append(html);
+    });
+
+    $(document).on("click", ".remove-catagory", function(e) {
+    	$(this).parent().parent().parent().remove();
+    });
+
+    $(document).on("click", ".add-catagory-course", function(e) {
+    	var html = '<tr>'+
+    	           '<td>' +
+    			   '<div class="add-course input-group mt-2 mb-2 ml-5">' +
+    			   '<div class="input-group-prepend">' +
+    			   '<span class="input-group-text" style="padding-right: .5em;">' +
+    			   '<i class="fa fa-trash remove-course mr-1"></i>Course Name</span>' +
+    			   '</div>' +
+    			   '<input type="text" class="form-control add-course-input">' +
+    			   '</div>' +
+    			   '</td>' +
+    	           '</tr>';
+    	$(this).parent().children(".catagory-courses").append(html);
+    });
+
+    $(document).on("click", ".remove-course", function(e) {
+    	$(this).parent().parent().parent().remove();
     });
 
 
